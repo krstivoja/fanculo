0d482c03ae4c7c03438bb6c53a0c022bb8cf92b6
Marko Krstic <mailme@markokrstic.com>
2025-10-04 09:30:58 +0200

    Update AdminButton.js

--- src/app/components/ui/AdminButton.js
+++ src/app/components/ui/AdminButton.js
@@ -2,9 +2,9 @@ import { LogoIcon, WordPressIcon } from "../icons";
 
 export default function AdminButton() {
   return (
-    <div
-      className="bg-contrast hover:bg-action text-base-1 hover:text-highlight p-3 w-fit cursor-pointer transition-all duration-200 hover:bg-opacity-80 group relative"
-      onClick={() => (window.location.href = "/wp-admin/")}
+    <a
+      href="/wp-admin/"
+      className="bg-contrast hover:bg-action !text-base-1 hover:!text-highlight p-3 w-fit cursor-pointer transition-all duration-200 hover:bg-opacity-80 group relative block"
       title="Go to WordPress Admin"
     >
       <div className="group-hover:opacity-0 ">
@@ -13,6 +13,6 @@ export default function AdminButton() {
       <div className="absolute inset-0 p-3 opacity-0 group-hover:opacity-100 ">
         <WordPressIcon size={32} />
       </div>
-    </div>
+    </a>
   );
 }
